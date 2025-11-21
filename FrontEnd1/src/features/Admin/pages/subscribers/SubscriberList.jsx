a44e05ac7a632cccf52b2b7ef936325f02803181
sujan sah <sujansah.dev@gmail.com>
2025-11-21 11:03:46 +0545

    fix auth ui + dark theme

---
+++ FrontEnd1/src/features/Admin/pages/subscribers/SubscriberList.jsx
@@ -0,0 +1,58 @@
+
+
+import Input from "@/features/ui/Input";
+import { DeleteIcon, Edit3Icon, EditIcon, FilterXIcon, SaveIcon, UsersIcon, UserPlusIcon } from "lucide-react";
+import React, { useState } from "react";
+import { NewUsers } from "./NewUsers"; 
+import { ExistingUsers } from "./ExistingUsers";
+import Button from "@/features/ui/Button";
+
+const SubscriberList = () => {
+  // 👥 Existing Subscribers Data
+ 
+
+  // State to manage which view is active: 'subscribers' or 'potential'
+  const [activeView, setActiveView] = useState('existing');
+  return (
+    <div className="min-w-2xl max-w-full mx-auto  text-muted-hover rounded-xl ">
+      <h1 className="text-2xl font-bold text-dark dark:text-white mb-5">Subscriber Management</h1>
+
+      {/* ↔️ Tab/Section Switcher */}
+      <div className="flex space-x-4 mb-6 border-b border-muted/40">
+        <button
+          onClick={() => {
+            setActiveView('existing');
+          }}
+      
+        >
+           <Button className={` ${activeView=="existing"? "bg-primary":"bg-primary/70"} `}>
+           <UsersIcon className="w-4 h-4" />
+          <span>Existing Subscribers</span>
+          </Button>
+         
+        </button>
+        <div
+          onClick={() => {
+            setActiveView('new');
+          }}
+         
+        >
+           <Button className={` ${activeView=="new"? "bg-primary":"bg-primary/70"} `}>
+          <UserPlusIcon className="w-4 h-4" />
+         <span>New/Potential Users </span> 
+          </Button>
+            </div>
+        
+      </div>
+
+      {/* 📋 Conditional Table Rendering */}
+      {activeView === 'existing' ? (
+        <ExistingUsers/>
+      ) : (
+        <NewUsers />
+      )}
+    </div>
+  );
+};
+
+export default SubscriberList;
--- FrontEnd1/src/globalPages/auth/Login.jsx
+++ FrontEnd1/src/globalPages/auth/Login.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 import { EyeIcon, EyeSlashIcon, TvIcon } from "@heroicons/react/24/outline";
 import { AlignRight, CheckIcon, PyramidIcon } from "lucide-react"; // AlignRight might not be directly used in the form, but keeping it as it was in your imports.
+import Input from "@/features/ui/Input";
+import Button from "@/features/ui/Button";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -74,133 +76,82 @@ const Login = () => {
   };
 
   return (
-    <div className="login min-h-screen w-full  bg-center bg-cover bg-no-repeat flex flex-col justify-center items-center">
-      {/* Main container for the two columns */}
-      <div className="w-full h-screen bg-white grid grid-cols-1 md:grid-cols-2 shadow-lg rounded-lg overflow-hidden ">
-        {/* left Column: Login Form */}
-        <div className=" items-center flex flex-col justify-center p-2">
-          <div className="w-full flex  mt-8 justify-around">
-            <p className="flex font-bold gap-2  ">
-              <PyramidIcon /> <span> SMART-BILL </span>
-            </p>
-            <select className="border-2  rounded-md bg-gray-300 hover:border-primary-green px-2 py-1">
-              <option className="font-bold text-black bg-gray-300" value="English">
-               English
-              </option>
-              <option value="Nepali">
-                नेपाली 
-              </option>
-            </select>
-          </div>
-
-          <div className="w-full md:w2/3 lg:w-2/3 mt-16 bg-white flex flex-col justify-center items-center ">
-            <div className="text-center ">
-              <div className=" mb-2"></div>
-              <h2 className=" text-3xl font-bold text-gray-900">
-                Lets Get Started
-              </h2>
-              <p className="text-gray-400 mt-4 ">Please Login to continue</p>
-             
+    <div className="login min-h-screen w-full flex flex-row-reverse">
+      <div className="left-section w-full p-2 flex items-center justify-center">
+      <div className="left-section w-full mx-auto max-w-120  flex flex-col justify-center items-center ">
+      <p className="flex items-center font-bold gap-2 text-3xl mb-4">
+        <PyramidIcon strokeWidth={2.5} /> <span> SMART-BILL </span>
+      </p>
+      <div className="text-center mb-3">
+      <p className="text-2xl font-semibold">Welcome Back!</p>  
+      <p className="text-muted text-sm">Sign in to access your account</p>
+      </div>
+    
+        <div className="mt-4 py-1 px-8  w-full">
+          {error && (
+            <div
+              className="mb-4 rounded-lg border border-destructive bg-destructive/10 px-4 py-3 text-destructive text-sm"
+            >
+              * {error}
             </div>
+          )}
 
-            <div className="mt-4 bg-white py-1 px-8  w-full shadow sm:rounded-lg ">
-              {error && (
-                <div className="mb-4 bg-red-50 border border-red-200 text-red-600 px-4 py-3 rounded">
-                  {error}
-                </div>
-              )}
+          {/* Password Login Form */}
+          {loginMethod === "password" && (
+            <form onSubmit={handlePasswordLogin} className="space-y-4">
+              <Input label=" Email address"  id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}/>
+                    <div className="password-section relative">
 
-              {/* Password Login Form */}
-              {loginMethod === "password" && (
-                <form onSubmit={handlePasswordLogin} className="space-y-2 ">
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Email address
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        required
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                      />
-                    </div>
-                  </div>
+              <Input label=" Password"  id="password"
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    autoComplete="current-password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}/>
 
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Password
-                    </label>
-                    <div className="mt-1 relative">
-                      <input
-                        id="password"
-                        name="password"
-                        type={showPassword ? "text" : "password"}
-                        autoComplete="current-password"
-                        required
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                      />
                       <button
-                        type="button"
-                        className="absolute inset-y-0 right-0 pr-3 flex items-center"
-                        onClick={() => setShowPassword(!showPassword)}
-                      >
-                        {showPassword ? (
-                          <EyeSlashIcon className="h-5 w-5 text-gray-400" />
-                        ) : (
-                          <EyeIcon className="h-5 w-5 text-gray-400" />
-                        )}
-                      </button>
+                    type="button"
+                    className="absolute inset-y-0 right-0 pt-7 pr-2 flex items-center"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? (
+                      <EyeSlashIcon className="h-5 w-5 text-gray-400" />
+                    ) : (
+                      <EyeIcon className="h-5 w-5 text-gray-400" />
+                    )}
+                  </button>
                     </div>
-                  </div>
 
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                     
-                    </div>
-                    <div className="text-sm">
-                      <Link
-                        to="/forgot-password"
-                        className="font-medium text-blue-600 hover:text-blue-500"
-                      >
-                        Forgot your password?
-                      </Link>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button
-                      type="submit"
-                      disabled={isLoading}
-                      className="w-full mt-5 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-primary-green focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50"
-                    >
-                      {isLoading ? "Signing in..." : "Sign in"}
-                    </button>
-                  </div>
-                </form>
-              )}
-            </div>
-          </div>
+              
+                
+                  <Link
+                    to="/forgot-password"
+                    className="text-right block font-medium text-primary hover:text-primary-hover"
+                  >
+                    Forgot your password?
+                  </Link>
+                  <Button  type="submit"
+                  disabled={isLoading} className="w-full">
+                     {isLoading ? "Signing in..." : "Sign in"}  
+                  </Button>
+            </form>
+          )}
         </div>
-        <div className="hidden md:flex items-center justify-center bg-green-400 p-8">
-          <img
+      </div>
+      </div>
+      <div className="right-section hidden md:block w-full bg-constructive">
+         <img
             src="https://images.unsplash.com/photo-1579547621113-e4d2719a4087?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" // Example image
             alt="Login Illustration"
             className="object-cover w-full h-full rounded-md"
           />
-        </div>
       </div>
     </div>
   );
